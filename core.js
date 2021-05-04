077b8f05ecff2c69273bf5df965a159fb353b50a
zookievookie <zakky.h.v@gmail.com>
2021-05-04 01:17:02 -0500

    wiki added

---
+++ core.js
@@ -0,0 +1,13 @@
+var i=0;
+function inc(){
+    document.getElementById('increase').innerHTML = i;
+    i++;
+}
+function listfold(listname,itemids){
+  
+}
+document.addEventListener("keypress", function keystroke(name) {
+    if(name.key==="b"){
+      //do something
+    }
+});
